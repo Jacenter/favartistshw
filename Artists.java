d5a3e80f88edf4b66c94cdd653d26a938d54d40c
Jacenter <JNovack1501@gmail.com>
2021-03-11 18:56:39 -0500

    Artists file

---
+++ Artists.java
@@ -0,0 +1,7 @@
+public class Artists {
+    public static void main(String[] args) {
+        System.out.println("Michaelangelo");
+        System.out.println("Vincent Van Gough");
+        System.out.println("Pablo Picasso");
+    }
+}
